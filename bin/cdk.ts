9c94fb9814b651aaa223a53f9f546aab1f4888e6
Clay Danford <clay_danford@mckinsey.com>
2022-10-01 00:59:54 -0500

    Add tags

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
-import { App } from 'aws-cdk-lib'
+import { App, Tags } from 'aws-cdk-lib'
 import { BucketStack } from '../lib/BucketStack'
 import { CrossRegionStack } from '../lib/CrossRegionStack'
 
@@ -10,4 +10,9 @@ const app = new App()
 
 new BucketStack(app, 'BucketStack', { paramName, env: { region: 'us-east-1' } })
 
-new CrossRegionStack(app, 'ParamStack', { paramName, env: { region: 'us-west-2' } })
+new CrossRegionStack(app, 'ParamStack', {
+  paramName,
+  env: { region: 'us-west-2' },
+})
+
+Tags.of(app).add('foo', 'bar')
